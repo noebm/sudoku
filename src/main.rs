cdc133e46d8e9e8bfe8b4fcd2538e895307abb25
Moritz Noebauer <moritz.noebauer@gmail.com>
2024-08-28 17:06:13 +0200

    finished solver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,213 @@
+// Cells are either a value or possible values!
+
+#[derive(Copy, Clone)]
+enum Cell<S, T> {
+    Value(S),
+    Possibly(T),
+}
+
+trait Possibilities {
+    type Value;
+    type Possibly;
+    fn all() -> Self::Possibly;
+    fn eliminate(&mut self, value: Self::Value); // -> Result<Self::Value, Self::Possibly>;
+}
+
+#[derive(Clone)]
+struct SudokuCell(Cell<u8, Vec<u8>>);
+
+impl SudokuCell {
+    fn finished(&self) -> bool {
+        matches!(self.0, Cell::Value(..))
+    }
+
+    fn get(&self) -> Option<u8> {
+        if let Cell::Value(value) = self.0 {
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
+impl Possibilities for SudokuCell {
+    type Value = u8;
+    type Possibly = Vec<u8>;
+
+    fn all() -> Self::Possibly {
+        (1..=9).collect()
+    }
+
+    fn eliminate(&mut self, value: Self::Value) {
+        if let Cell::Possibly(values) = &mut self.0 {
+            // FIXME this only works for elements contained at most 1 time!
+            values
+                .iter()
+                .position(|&x| x == value)
+                .map(|index| values.remove(index));
+
+            assert!(!values.is_empty());
+
+            if values.len() == 1 {
+                self.0 = Cell::Value(values[0])
+            }
+        }
+    }
+}
+
+struct Sudoku {
+    board: Vec<SudokuCell>,
+}
+
+impl TryFrom<String> for Sudoku {
+    type Error = ();
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.len() != 81 {
+            return Err(());
+        }
+
+        let mut board = Sudoku::new();
+
+        for (idx, c) in value.chars().enumerate() {
+            if let '1'..='9' = c {
+                board.board[idx] = SudokuCell(Cell::Value(c.to_string().parse().map_err(|_| ())?));
+            }
+        }
+        Ok(board)
+    }
+}
+
+impl std::fmt::Display for Sudoku {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for x in self.board.clone() {
+            if let Cell::Value(value) = x.0 {
+                write!(f, "{value}")?;
+            } else {
+                write!(f, "0")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Sudoku {
+    fn new() -> Self {
+        let cell = SudokuCell(Cell::Possibly(SudokuCell::all()));
+        Self {
+            board: vec![cell.clone(); 81],
+        }
+    }
+
+    fn finished(&self) -> bool {
+        self.board.iter().all(|x| x.finished())
+    }
+
+    fn index(row: usize, column: usize) -> usize {
+        row * 9 + column
+    }
+
+    fn solve_rows(&mut self) {
+        for row in 0..9 {
+            let values: Vec<_> = (0..9)
+                .filter_map(|column| self.board[Self::index(row, column)].get())
+                .collect();
+
+            for value in values {
+                for column in 0..9 {
+                    self.board[Self::index(row, column)].eliminate(value)
+                }
+            }
+        }
+    }
+
+    fn solve_columns(&mut self) {
+        for column in 0..9 {
+            let values: Vec<_> = (0..9)
+                .filter_map(|row| self.board[Self::index(row, column)].get())
+                .collect();
+
+            for value in values {
+                for row in 0..9 {
+                    self.board[Self::index(row, column)].eliminate(value)
+                }
+            }
+        }
+    }
+
+    fn box_indices(box_index: usize) -> [usize; 9] {
+        let mut indices = [0; 9];
+        let mut i = 0;
+        for row in 0..3 {
+            let base_row = box_index % 3;
+            for column in 0..3 {
+                let base_column = box_index / 3;
+                indices[i] = Self::index(base_row * 3 + row, base_column * 3 + column);
+                i += 1;
+            }
+        }
+
+        indices
+    }
+
+    fn solve_boxes(&mut self) {
+        for indices in (0..9).map(Self::box_indices) {
+            let values: Vec<_> = indices
+                .iter()
+                .filter_map(|&idx| self.board[idx].get())
+                .collect();
+
+            for value in values {
+                for idx in indices {
+                    self.board[idx].eliminate(value)
+                }
+            }
+        }
+    }
+
+    fn step(&mut self) {
+        self.solve_rows();
+        self.solve_columns();
+        self.solve_boxes();
+    }
+
+    fn solve(&mut self) {
+        let mut step_count = 0;
+        while !self.finished() {
+            self.step();
+            step_count += 1;
+            if step_count > 81 {
+                println!("Could not solve board. Aborting!");
+                break;
+            }
+        }
+    }
+}
+
+const BOARD: &str = "
+003020600
+900305001
+001806400
+008102900
+700000008
+006708200
+002609500
+800203009
+005010300
+";
+
 fn main() {
-    println!("Hello, world!");
+    println!("{BOARD}");
+    println!();
+
+    let board_fixed: String = BOARD.to_string().lines().collect();
+
+    let mut sudoku = Sudoku::try_from(board_fixed).unwrap();
+    println!("{sudoku}\n");
+
+    sudoku.solve();
+    println!("{sudoku}\n");
+
+    // Sudoku::try_from(BOARD.to_string().lines().collect())
+
+    // println!("Hello, world!");
 }
